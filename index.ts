784c74538c8c454bfaa63d3812938799a6869128
Afonso -aptsharp <aff-on@hotmail.com>
2021-07-25 19:44:37 -0300

    types

--- index.ts
+++ index.ts
@@ -1,26 +1,36 @@
 //types
+
+// const input = document.getElementById('input') as HTMLInputElement;
+
+// input.addEventListener('input', (event) => {
+//     const i = event.currentTarget as HTMLInputElement;
+//     console.log(i.value);
+// });
+
+
+//types
 //interface
 
-interface IAnimal {
-  nome: string;
-  tipo: "terrestre" | "aquatico";
-  domestico: boolean;
-}
+// interface IAnimal {
+//   nome: string;
+//   tipo: "terrestre" | "aquatico";
+//   domestico: boolean;
+// }
 
-interface IFelino extends IAnimal {
-  visaoNoturna: boolean;
-}
+// interface IFelino extends IAnimal {
+//   visaoNoturna: boolean;
+// }
 
-interface ICanino extends IAnimal {
-  porte: "pequeno" | "medio" | "grande";
-}
+// interface ICanino extends IAnimal {
+//   porte: "pequeno" | "medio" | "grande";
+// }
 
-type IDomestico = IFelino & ICanino;
+// type IDomestico = IFelino & ICanino;
 
-const animal: IDomestico = {
-  domestico: true,
-  nome: "cachorro",
-  porte: "medio",
-  tipo: "terrestre",
-  visaoNoturna: true,
-};
+// const animal: IDomestico = {
+//   domestico: true,
+//   nome: "cachorro",
+//   porte: "medio",
+//   tipo: "terrestre",
+//   visaoNoturna: true,
+// };
