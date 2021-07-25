2427ad9c64399b01ebc94e9fb323dd411ab14f7f
Afonso -aptsharp <aff-on@hotmail.com>
2021-07-25 19:23:24 -0300

    exemplos de type

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log("TypeScript");
+//types
+//interface
+
+interface IAnimal {
+  nome: string;
+  tipo: "terrestre" | "aquatico";
+  domestico: boolean;
+}
+
+interface IFelino extends IAnimal {
+  visaoNoturna: boolean;
+}
+
+interface ICanino extends IAnimal {
+  porte: "pequeno" | "medio" | "grande";
+}
+
+type IDomestico = IFelino & ICanino;
+
+const animal: IDomestico = {
+  domestico: true,
+  nome: "cachorro",
+  porte: "medio",
+  tipo: "terrestre",
+  visaoNoturna: true,
+};
